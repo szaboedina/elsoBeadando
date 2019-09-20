70c102799d468bf6b07275c58eebe508d95a77d5
Edina Szabó <szabo.edina.1229@gmail.com>
2019-09-20 12:23:26 +0200

    most mar tenyleg az elso feladat

--- Program.cs
+++ Program.cs
@@ -9,7 +9,7 @@ namespace ConsoleApp3
     class Program
     {
         static int[] szamok = new int[10];
-        static void Elso()
+        static void Beker()
         {   
             Console.WriteLine("Kérem adjon meg 10 egész számot");
             int bekert;
@@ -19,15 +19,32 @@ namespace ConsoleApp3
                 szamok[i] = bekert;
             }
         }
+        
 
         static void Main(string[] args)
         {
-            Elso();
+            Beker();
             for (int i = 0; i < szamok.Length; i++)
             {
                 Console.Write(szamok[i]+"   ");
-
             }
+            Console.WriteLine();
+
+            int max = szamok[0];
+            for (int i = 1; i < szamok.Length; i++)
+            {
+                if (max<szamok[i])
+                {
+                    max = szamok[i];
+                }
+            }
+            Console.WriteLine("A legnagyobb szám : "+max);
+
+
+
+
+
+
             Console.ReadKey();
         }
     }
