113eec5bb516d5275d6f16f196f4889046c91560
Edina Szabó <szabo.edina.1229@gmail.com>
2019-09-20 12:29:50 +0200

    masodik feladat

--- Program.cs
+++ Program.cs
@@ -7,16 +7,18 @@ using System.Threading.Tasks;
 namespace ConsoleApp3
 {
     class Program
-    {
-        static int[] szamok = new int[10];
+    {   
+        static List<int> szamok= new List<int>();
         static void Beker()
         {   
-            Console.WriteLine("Kérem adjon meg 10 egész számot");
+            Console.WriteLine("Kérem adjon meg hány számot szeretne felvenni:");
+            int hossz = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Adja meg a számokat:");
             int bekert;
-            for (int i = 0; i < szamok.Length; i++)
+            for (int i = 0; i < hossz; i++)
             {
                 bekert = Convert.ToInt32(Console.ReadLine());
-                szamok[i] = bekert;
+                szamok.Add(bekert);
             }
         }
         
@@ -24,14 +26,14 @@ namespace ConsoleApp3
         static void Main(string[] args)
         {
             Beker();
-            for (int i = 0; i < szamok.Length; i++)
+            for (int i = 0; i < szamok.Count; i++)
             {
                 Console.Write(szamok[i]+"   ");
             }
             Console.WriteLine();
 
             int max = szamok[0];
-            for (int i = 1; i < szamok.Length; i++)
+            for (int i = 1; i < szamok.Count; i++)
             {
                 if (max<szamok[i])
                 {
@@ -43,7 +45,7 @@ namespace ConsoleApp3
 
 
 
-
+            
 
             Console.ReadKey();
         }
